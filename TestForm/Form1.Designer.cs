72527cde94d4fe0cd38257b53bc97629b65c651e
Vepur Saicharan <Vepur Saicharan@VEPUR-SAICHARAN>
2024-07-24 10:58:37 +0200

    Add project files.

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadPDF = new System.Windows.Forms.Button();
+            this.pdfPictureBox = new System.Windows.Forms.PictureBox();
+            this.btnCaptureSignature = new System.Windows.Forms.Button();
+            this.signaturePictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSavePDF = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pdfPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signaturePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadPDF
+            // 
+            this.btnLoadPDF.Location = new System.Drawing.Point(73, 74);
+            this.btnLoadPDF.Name = "btnLoadPDF";
+            this.btnLoadPDF.Size = new System.Drawing.Size(163, 23);
+            this.btnLoadPDF.TabIndex = 0;
+            this.btnLoadPDF.Text = "Load PDF";
+            this.btnLoadPDF.UseVisualStyleBackColor = true;
+            this.btnLoadPDF.Click += new System.EventHandler(this.btnLoadPDF_Click);
+            // 
+            // pdfPictureBox
+            // 
+            this.pdfPictureBox.Location = new System.Drawing.Point(55, 124);
+            this.pdfPictureBox.Name = "pdfPictureBox";
+            this.pdfPictureBox.Size = new System.Drawing.Size(204, 268);
+            this.pdfPictureBox.TabIndex = 1;
+            this.pdfPictureBox.TabStop = false;
+            // 
+            // btnCaptureSignature
+            // 
+            this.btnCaptureSignature.Location = new System.Drawing.Point(445, 107);
+            this.btnCaptureSignature.Name = "btnCaptureSignature";
+            this.btnCaptureSignature.Size = new System.Drawing.Size(91, 23);
+            this.btnCaptureSignature.TabIndex = 2;
+            this.btnCaptureSignature.Text = "Capture";
+            this.btnCaptureSignature.UseVisualStyleBackColor = true;
+            this.btnCaptureSignature.Click += new System.EventHandler(this.btnCaptureSignature_Click);
+            // 
+            // signaturePictureBox
+            // 
+            this.signaturePictureBox.Location = new System.Drawing.Point(370, 136);
+            this.signaturePictureBox.Name = "signaturePictureBox";
+            this.signaturePictureBox.Size = new System.Drawing.Size(240, 87);
+            this.signaturePictureBox.TabIndex = 3;
+            this.signaturePictureBox.TabStop = false;
+            // 
+            // btnSavePDF
+            // 
+            this.btnSavePDF.Location = new System.Drawing.Point(445, 348);
+            this.btnSavePDF.Name = "btnSavePDF";
+            this.btnSavePDF.Size = new System.Drawing.Size(91, 23);
+            this.btnSavePDF.TabIndex = 5;
+            this.btnSavePDF.Text = "Save PDF";
+            this.btnSavePDF.UseVisualStyleBackColor = true;
+            this.btnSavePDF.Click += new System.EventHandler(this.btnSavePDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSavePDF);
+            this.Controls.Add(this.signaturePictureBox);
+            this.Controls.Add(this.btnCaptureSignature);
+            this.Controls.Add(this.pdfPictureBox);
+            this.Controls.Add(this.btnLoadPDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pdfPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.signaturePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadPDF;
+        private System.Windows.Forms.PictureBox pdfPictureBox;
+        private System.Windows.Forms.Button btnCaptureSignature;
+        private System.Windows.Forms.PictureBox signaturePictureBox;
+        private System.Windows.Forms.Button btnSavePDF;
+
+    }
+}
+
---
+++ TestForm/Form1.cs
@@ -0,0 +1,180 @@
+﻿using PdfiumViewer;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using wgssSTU;
+using TabletLib;
+using GraphicsLib; 
+using System.Collections.Generic;
+using DemoButtons;
+
+
+
+enum PenDataOptionMode
+{
+    PenDataOptionMode_None,
+    PenDataOptionMode_TimeCount,
+    PenDataOptionMode_SequenceNumber,
+    PenDataOptionMode_TimeCountSequence
+};
+
+
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        private string pdfPath;
+        private wgssSTU.Tablet m_tablet;
+        private wgssSTU.ICapability m_capability;
+        private wgssSTU.IInformation m_information;
+        private wgssSTU.IProtocolHelper m_protocolHelper;
+        private Bitmap m_bitmap;
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+            m_protocolHelper = new wgssSTU.ProtocolHelper();
+        }
+
+        private void btnLoadPDF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pdfPath = openFileDialog.FileName;
+
+                // Render PDF to Bitmap using PdfiumViewer
+                using (var document = PdfDocument.Load(pdfPath))
+                {
+                    var page = document.Render(0, 300, 300, true); // Render the first page at 300 DPI
+                    pdfPictureBox.Image = page;
+                }
+            }
+        }
+
+
+        private void btnCaptureSignature_Click(object sender, EventArgs e)
+        {
+            int penDataType;
+            List<IPenDataTimeCountSequence> penTimeData = null;
+            List<IPenData> penData = null;
+
+            UsbDevices usbDevices = new UsbDevices();
+            if (usbDevices.Count != 0)
+            {
+                try
+                {
+                    IUsbDevice usbDevice = usbDevices[0]; // select a device
+
+                    SignatureForm demo = new SignatureForm(usbDevice, false); // Assuming no encryption for simplicity
+                    demo.ShowDialog();
+                    penDataType = demo.penDataType;
+
+                    if (penDataType == (int)PenDataOptionMode.PenDataOptionMode_TimeCountSequence)
+                        penTimeData = demo.getPenTimeData();
+                    else
+                        penData = demo.getPenData();
+
+                    if (penData != null || penTimeData != null)
+                    {
+                        m_capability = demo.getCapability();
+                        m_information = demo.getInformation();
+
+                        if (penData != null)
+                        {
+                            DisplaySignature(penData);
+                        }
+                        else
+                        {
+                            DisplaySignature(penTimeData);
+                        }
+                    }
+                    demo.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No STU devices attached");
+            }
+        }
+
+        // Method to display the signature using your graphics library
+        private void DisplaySignature(List<IPenData> penData)
+        {
+            m_bitmap = new Bitmap(m_capability.screenWidth, m_capability.screenHeight);
+            using (Graphics gfx = Graphics.FromImage(m_bitmap))
+            {
+                gfx.Clear(Color.White);
+                foreach (var data in penData)
+                {
+                    if (data.sw > 0)
+                    {
+                        Point pt = new Point(data.x, data.y);
+                        gfx.DrawLine(Pens.Black, pt, pt); // Use appropriate logic to draw lines
+                    }
+                }
+            }
+            signaturePictureBox.Image = m_bitmap;
+        }
+
+        // Overloaded method for IPenDataTimeCountSequence
+        private void DisplaySignature(List<IPenDataTimeCountSequence> penTimeData)
+        {
+            m_bitmap = new Bitmap(m_capability.screenWidth, m_capability.screenHeight);
+            using (Graphics gfx = Graphics.FromImage(m_bitmap))
+            {
+                gfx.Clear(Color.White);
+                foreach (var data in penTimeData)
+                {
+                    if (data.sw > 0)
+                    {
+                        Point pt = new Point(data.x, data.y);
+                        gfx.DrawLine(Pens.Black, pt, pt); // Use appropriate logic to draw lines
+                    }
+                }
+            }
+            signaturePictureBox.Image = m_bitmap;
+        }
+
+
+        private void btnSavePDF_Click(object sender, EventArgs e)
+        {
+            if (signaturePictureBox.Image != null && pdfPictureBox.Image != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string savePath = saveFileDialog.FileName;
+                    PdfDocument document = PdfReader.Open(pdfPath, PdfDocumentOpenMode.Modify);
+                    PdfPage page = document.Pages[0];
+                    XGraphics gfx = XGraphics.FromPdfPage(page);
+
+                    // Save the signature image to a temporary file and then load it back as XImage
+                    string tempPath = Path.GetTempFileName();
+                    signaturePictureBox.Image.Save(tempPath, System.Drawing.Imaging.ImageFormat.Png);
+                    XImage xImage = XImage.FromFile(tempPath);
+
+                    // Specify the position where the signature should be placed
+                    int xPosition = 100; // Example x position
+                    int yPosition = 100; // Example y position
+                    gfx.DrawImage(xImage, xPosition, yPosition, xImage.PixelWidth, xImage.PixelHeight);
+
+                    document.Save(savePath);
+                    MessageBox.Show("PDF saved successfully.");
+
+                    // Clean up temporary file
+                    File.Delete(tempPath);
+                }
+            }
+        }
+    }
+}
